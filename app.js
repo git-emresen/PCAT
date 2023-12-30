66b967ab433f64ceea4ef0f19e3f791985296a97
emresen <mail.emresen@gmail.com>
2023-12-30 20:26:33 +0300

    edit a photo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const fs=require('fs')
 const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
+const methodOverride=require('method-override')
 const path = require('path')
 const ejs = require('ejs')
 const Photo = require('./models/Photo')
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(fileUpload())
 app.set("view engine", "ejs")
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
    const photos = await Photo.find({}).sort('-dateCreated')
@@ -37,6 +39,24 @@ app.get('/photos/:id', async (req, res) => {
    res.render("photo", {
       photo
    })
+})
+
+app.get('/photos/edit/:id',async (req,res)=>{
+   const photo=await Photo.findOne({_id: req.params.id})
+   res.render("edit",{
+      photo
+   })
+})
+
+app.put('/photos/:id', async (req,res)=>{
+   const photo=await Photo.findOne({_id: req.params.id})
+   photo.title=req.body.title
+   photo.description=req.body.description
+   photo.save()
+   res.render("edit",{
+      photo
+   })
+  /*  res.redirect(`/photos/${req.params.id}' */
 })
 
 app.post('/photos', async (req, res) => { 
